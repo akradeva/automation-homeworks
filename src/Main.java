4e392d38e4e4e6a4ea410885f2a6fcb40a260904
akradeva <anna-kristina.radeva@mentormate.com>
2022-04-06 17:32:54 +0300

    Homework 1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // 1. Store Names in three variables
+        StoreNames_1.PrintNames();
+
+        System.out.println("----------end----------");
+
+        // 2. Print the perimeter of a triangle by given sides
+        TrianglePerimeter_2.TrianglePerimeter();
+
+        System.out.println("----------end----------");
+
+        // 3 . Print the area of a triangle by given sides
+        TriangleArea_3.TriangleArea();
+
+        System.out.println("----------end----------");
+
+        // 4. Print a pine tree
+        PineTree_4.pineTree();
+
+        System.out.println("----------end----------");
+
+        // 5.1. Refine task 2: Triangle perimeter
+        JavaInput_5.JavaInputTrianglePerimeter();
+
+        System.out.println("----------end----------");
+
+        // 5.2. Refine task 3: Triangle area
+        JavaInput_5.JavaInputTriangleArea();
+
+        System.out.println("----------end----------");
+
+        // 6. Quadratic Equation
+        QuadraticEquation.QuadraticEquation();
+    }
+}
