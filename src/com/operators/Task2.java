60e437bd9ad8d54e1ba8fe47d3b616cb758aaef8
akradeva <anna-kristina.radeva@mentormate.com>
2022-04-11 20:44:29 +0300

    Task 2: Write Java program to allow the user to input his/her age. Then the program will show if the person is eligible to work. A person who is eligible to work must be older than or equal to 16 years old.

--- src/com/operators/Task2.java
+++ src/com/operators/Task2.java
@@ -1,7 +1,21 @@
 package com.operators;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please, enter your age: ");
+        int age = myScanner.nextInt();
+
+        if (age >= 16) {
+            System.out.println("You are eligible to work.");
+        } else {
+            System.out.println("You are NOT eligible to work.");
+        }
+
+
+
 
     }
 }
