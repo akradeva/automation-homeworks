4efd366007d23e94517a0509ba44291f08fae4af
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-17 23:51:20 +0300

    Task 7: Reverse the following list using for loop list1 = [10, 20, 30, 40, 50]

--- src/com/operators/Task7.java
+++ src/com/operators/Task7.java
@@ -1,2 +1,18 @@
-package com.operators;public class Task7 {
+package com.operators;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task7 {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList(Arrays.asList(10, 20, 30, 40, 50));
+        List<Integer> newList = new ArrayList<>();
+
+        for (int i = list1.size()-1; i >= 0; i--){
+            newList.add(list1.get(i));
+        }
+        System.out.println("The given list: " + list1.toString());
+        System.out.println("The reversed list: " + newList.toString());
+    }
 }
