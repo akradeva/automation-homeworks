93e5b9d8db0d815cf17df0454f3d3b24671bb6ca
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-17 23:47:24 +0300

    Task 5: A program which advices the user where to go to a vacation based on the type of the vacation and the budget.

--- src/com/operators/Task5.java
+++ src/com/operators/Task5.java
@@ -1,2 +1,32 @@
-package com.operators;public class Task5 {
+package com.operators;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a destination: ");
+        String destination = myScanner.nextLine();
+        System.out.println("Enter a budget per day per person: ");
+        int budget = myScanner.nextInt();
+
+        switch(destination){
+            case "Beach":
+                if (budget < 50){
+                    System.out.println("My advise is to choose Bulgaria as a destination.");
+                } else {
+                    System.out.println("You can afford to go outside Bulgaria as a destination.");
+                }
+                break;
+            case "Mountain":
+                if (budget < 30){
+                    System.out.println("My advise is to choose Bulgaria as a destination.");
+                } else {
+                    System.out.println("You can afford to go outside Bulgaria as a destination.");
+                }
+                break;
+            default:
+                System.out.println("There is no information about this type of vacation.");
+        }
+    }
 }
