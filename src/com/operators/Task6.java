f4a02ee047f49a073bd8fa5b0cab8396f3177fba
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-17 23:50:30 +0300

    Task 6: Iterate a given list and display numbers which are divisible by 5.

--- src/com/operators/Task6.java
+++ src/com/operators/Task6.java
@@ -1,2 +1,36 @@
-package com.operators;public class Task6 {
+package com.operators;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task6 {
+    public static void main(String[] args) {
+
+        //Solution with a list:
+
+        ArrayList<Integer> list1 = new ArrayList(Arrays.asList(12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200));
+
+        for (int i = 0; i < list1.size(); i++){
+            if (list1.get(i) % 5 == 0){
+                if (list1.get(i) > 150){
+                    break;
+                }
+                System.out.println(list1.get(i));
+            }
+        }
+
+
+        //Solution with an array:
+
+//        int[] arr = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+//
+//        for (int i = 0; i < arr.length; i++){
+//            if (arr[i] % 5 == 0){
+//                if (arr[i] > 150){
+//                    break;
+//                }
+//                System.out.println(arr[i]);
+//            }
+//        }
+    }
 }
