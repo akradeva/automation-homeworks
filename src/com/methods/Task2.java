847a2b15ae19293bb7c0adc94096caf71fb527cc
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-29 19:44:19 +0300

    Task 2: Write a Java method to compute the average of three numbers.

--- src/com/methods/Task2.java
+++ src/com/methods/Task2.java
@@ -1,2 +1,12 @@
-package com.methods;public class Task2 {
+package com.methods;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("The average of the entered numbers is: " +
+                String.format("%.2f", computeAverageNum(1, 5, 8)));
+    }
+
+    static double computeAverageNum(double firstNum, double secondNum, double thirdNum) {
+        return (firstNum + secondNum + thirdNum) / 3.0;
+    }
 }
