52260332ea639579e23f99f6c53e9c6044987aba
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-29 19:48:33 +0300

    Task 5: A takes as input a non-negative integer and returns true if the number is a palindrome;

--- src/com/methods/Task5.java
+++ src/com/methods/Task5.java
@@ -1,2 +1,35 @@
-package com.methods;public class Task5 {
+package com.methods;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int input = myScanner.nextInt();
+
+        if (input < 0) {
+            System.out.println("Invalid input. The input must be a positive number.");
+        } else if (isPalindrome(input)) {
+            System.out.println("The entered number '" + input + "' is a palindrome.");
+        } else {
+            System.out.println("The entered number '" + input + "' is NOT a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(int number) {
+
+        int input = number;
+        int reverse = 0;
+
+        while (input > 0) {
+            int lastDigit = input % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            input /= 10;
+        }
+
+        return number == reverse;
+        //return (number == reverse) ? true : false;
+    }
 }
