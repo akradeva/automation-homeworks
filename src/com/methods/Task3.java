ca0a2c70bab573a083e954a57313e9670fb200a6
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-29 19:45:28 +0300

    Task 3: Write a Java method to display the middle character of a string.

--- src/com/methods/Task3.java
+++ src/com/methods/Task3.java
@@ -1,2 +1,25 @@
-package com.methods;public class Task3 {
+package com.methods;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = myScanner.nextLine();
+
+        displayMiddleChars(input);
+    }
+
+    public static void displayMiddleChars(String input) {
+
+        System.out.println("The middle character(s) of the entered string '" + input + "' is(are): ");
+
+        if (input.length() % 2 == 0) {
+            System.out.println(input.charAt(input.length() / 2));
+            System.out.println(input.charAt((input.length() / 2) - 1));
+        } else {
+            System.out.println(input.charAt(input.length() / 2));
+        }
+    }
 }
