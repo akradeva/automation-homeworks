0e097a9ea7d3ff414c34abc1a843bcf02605a444
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-29 19:47:24 +0300

    Task 4: Write a method named isEven that accepts an int argument. The method should return true if the argument is even, or false otherwise.

--- src/com/methods/Task4.java
+++ src/com/methods/Task4.java
@@ -1,2 +1,18 @@
-package com.methods;public class Task4 {
+package com.methods;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int num = myScanner.nextInt();
+
+        System.out.println(isEven(num)) ;
+    }
+
+    public static boolean isEven (int number) {
+        return number % 2 == 0;
+        //return number % 2 == 0 ? true: false;
+    }
 }
