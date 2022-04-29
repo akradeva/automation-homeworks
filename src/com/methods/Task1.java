01922f57ec5a8929837f883868151781dea77c1b
Anna-Kristina Radeva <anna-kristina.radeva@mentormate.com>
2022-04-29 19:43:24 +0300

    Task 1: Write a Java method to find the smallest number among three numbers

--- src/com/methods/Task1.java
+++ src/com/methods/Task1.java
@@ -1,33 +1,45 @@
 package com.methods;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
-        int firstNum = myScanner.nextInt();
-        System.out.println("Enter the second number: ");
-        int secondNum = myScanner.nextInt();
-        System.out.println("Enter the third number: ");
-        int thirdNum = myScanner.nextInt();
 
-        System.out.println(findSmallestNum(firstNum, secondNum, thirdNum));
+        System.out.println("The smallest from all entered numbers is: " + findSmallestNum());
 
     }
 
-    public static int findSmallestNum(int firstNum, int secondNum, int thirdNum) {
-        int smalestNum = Integer.MAX_VALUE;
-        for (int i = 0; i < 3; i++) {
-            if (smalestNum > i) {
-                //smalestNum = [i];
-            }
-//            ArrayList<Integer> numbers = new ArrayList<>(3);
-//            numbers.sort();
-//
-            return smalestNum;
+    public static int findSmallestNum() {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the count of the numbers you want to check from: ");
+        int smallestNum = Integer.MAX_VALUE;
+        int count = myScanner.nextInt();
+
+        ArrayList<Integer> numbers = new ArrayList<>(count);
+
+        for (int i=0; i<count; i++){
+            System.out.println("Enter a number: ");
+            int inputNum = myScanner.nextInt();
+            numbers.add(i, inputNum);
         }
-        return smalestNum;
+        // with foreach:
+        for (Integer number : numbers) {
+            if (smallestNum > number) {
+                smallestNum = number;
+            }
+        }
+
+        // with for loop:
+//        for (int j = 0; j < numbers.size(); j++){
+//            if (smallestNum > numbers.get(j)) {
+//                smallestNum = numbers.get(j);
+//            }
+//        }
+
+        return smallestNum;
     }
 }
 
